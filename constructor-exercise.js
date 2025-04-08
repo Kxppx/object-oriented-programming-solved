502ab4aad84e6b5008ed331c69c13c46eacfce43
Kxppx <kappa.x@hotmail.com>
2025-04-08 00:57:01 -0500

    Completed all requirements

--- constructor-exercise.js
+++ constructor-exercise.js
@@ -1,1 +1,39 @@
-/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */ 
+/* Please read the instructions located in the 1-constructor-exercise-readme.md file and complete your code below */
+function Book(title, author, year, genre) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.genre = genre;
+}
+
+Book.prototype.getSummary = function () {
+  return `The Book: ${this.title}, was made in ${this.year} by ${this.author}.`;
+};
+Book.prototype.isClassic = function () {
+  if (2025 - this.year > 50) {
+    return true;
+  } else {
+    return false;
+  }
+};
+Book.prototype.updateYear = function (newYear) {
+  this.year = newYear;
+  console.log(`Publication year updated to ${this.year}`);
+};
+Book.prototype.changeGenre = function (newGenre) {
+  this.genre = newGenre;
+  console.log(`Genre updated to ${this.genre}`);
+};
+
+const book1 = new Book("To Kill a Mockingbird", "Harper Lee", 1960, "Fiction");
+console.log(book1);
+
+console.log(book1.getSummary());
+
+console.log(book1.isClassic());
+
+book1.updateYear(2023);
+console.log(book1.getSummary());
+
+book1.changeGenre("Horror");
+console.log(book1);
--- oop-exercise.js
+++ oop-exercise.js
@@ -1,1 +1,90 @@
 /* Please read the instructions located in the exercises/2-oop-exercise-readme.md file and complete your code below */
+class Student {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.grades = [];
+  }
+  addGrade(grade) {
+    if (0 >= grade <= 100) {
+      this.grades.push(grade);
+    } else {
+      console.log("Grade must be between 0 and 100");
+    }
+  }
+  calculateAverageGrade() {
+    if (this.grades.length > 0) {
+      return this.grades.reduce(
+        (acc, val) => acc + val / this.grades.length,
+        0
+      );
+    } else {
+      return 0;
+    }
+  }
+  hasPassed(passingGrade) {
+    let average = this.calculateAverageGrade();
+    if (average >= passingGrade) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+}
+
+class Course {
+  constructor(title) {
+    this.title = title;
+    this.students = [];
+  }
+  enrollStudent(student) {
+    this.students.push(student);
+  }
+  listStudents() {
+    console.log(this.students);
+  }
+  calculateCourseAverage() {
+    if (this.students.length === 0) {
+      return 0;
+    } else {
+      let totalGrades = 0;
+      let gradeCount = 0;
+      for (let student of this.students) {
+        totalGrades += student.grades.reduce((acc, val) => acc + val, 0);
+        gradeCount += student.grades.length;
+      }
+      return totalGrades / gradeCount;
+    }
+  }
+}
+
+const student1 = new Student("Jose", 27);
+const student2 = new Student("Otto", 27);
+const student3 = new Student("Arya", 28);
+
+student1.addGrade(100);
+student1.addGrade(90);
+student1.addGrade(20);
+student2.addGrade(70);
+student2.addGrade(80);
+student2.addGrade(95);
+student3.addGrade(100);
+student3.addGrade(98);
+student3.addGrade(92);
+console.log(student1.calculateAverageGrade());
+console.log(student1.hasPassed(70));
+console.log(student1.getName());
+console.log(student1.getAge());
+
+const mathClass = new Course("Math");
+mathClass.enrollStudent(student1);
+mathClass.enrollStudent(student2);
+mathClass.enrollStudent(student3);
+mathClass.listStudents();
+console.log(mathClass.calculateCourseAverage());
